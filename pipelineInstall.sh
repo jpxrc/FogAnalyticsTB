b78792ddea0610a8b1ab7be93de6bb95425582c0
JP <jpxrc@mail.missouri.edu>
2018-01-20 16:22:54 -0600

    Updated script

--- pipelineInstall.sh
+++ pipelineInstall.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+wget=/usr/bin/wget
 sudo apt-get update
 sudo apt-get install python-pip
 sudo pip install opencv-python==3.1.0.0
@@ -7,20 +9,20 @@ sudo apt-get install  python-tk
 sudo pip install  matplotlib
 sudo apt-get install zip unzip
 
-wget https://github.com/junostar/FogAnalyticsTB/archive/master.zip
+$wget "https://github.com/junostar/FogAnalyticsTB/archive/master.zip"
 #wget https://github.com/junostar/FogAnalyticsTB.zip
 unzip FogAnalyticsTB.zip
-rm FogAnalyticsTB.zip
+sudo rm FogAnalyticsTB.zip
 cd lib
-wget https://github.com/tensorflow/models/archive/master.zip
+$wget "https://github.com/tensorflow/models/archive/master.zip"
 unzip master.zip
 rm master.zip
-cp -r models-master/research api
+sudo cp -r models-master/research api
 cd api 
 protoc object_detection/protos/*.proto --python_out=.
 
 cd ..
-rm -r models-master/
+sudo rm -r models-master/
 cd ..
 
-python pipeline.py
+python pipeline.py test
